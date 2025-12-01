f48e598f2454350c978a7756367cd926f82c9d64
Lucky111 <benjaminmillalonc@gmail.com>
2025-11-30 22:42:49 -0300

    casi

--- Billing_Backend/config/db_config.py
+++ Billing_Backend/config/db_config.py
@@ -1,11 +1,12 @@
-import os
+# Billing_Backend/config/db_config.py
 import psycopg2
+import os
 from psycopg2.extras import RealDictCursor
 
 def get_db():
     conn = psycopg2.connect(
         host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_SERVICE"),
+        database=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         port=os.getenv("DB_PORT")
